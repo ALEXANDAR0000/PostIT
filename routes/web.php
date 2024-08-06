b4cbb65b8ad2f62cfa69ec4ab09d7429cf97d178
Aleksandar Jocovic <jocovica1@gmail.com>
2024-08-06 18:36:24 +0200

    added update and delete

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+
 
 
 /*
@@ -17,10 +19,16 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    $posts= Post::all();
+    $posts=Post::all();
+   /* $posts=[];
+    if(auth()->check()){
+    
+  // $posts= auth()->user()->usersCoolPosts()->latest()->get();
+   $posts=Post::where('user_id',auth()->id())->get();
+    }*/
     return view ('home',['posts'=>$posts]);
 
-    return view('home');
+    
 });
 
 
@@ -32,3 +40,6 @@ Route ::post('/login', [UserController::class,'login']);
 
 //Blog post related routes
 Route ::post('/create-post', [PostController::class,'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
